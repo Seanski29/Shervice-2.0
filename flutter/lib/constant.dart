b5a6e53beb98c3c477d3bd151cadc0c2c381802a
Zitronus <lmn.mdz1@gmail.com>
2026-09-19 13:30:55 +0800

    constant.dart fix for railway

--- flutter/lib/constant.dart
+++ flutter/lib/constant.dart
@@ -1,4 +1,5 @@
-const String localIp = 'http://localhost:5000';
-const String backendUrl = 'http://localhost:5000/api';
-// const String localIp = 'https://shervice.onrender.com';
-// const String backendUrl = 'https://shervice.onrender.com/api';
+// const String localIp = 'http://localhost:5000';
+// const String backendUrl = 'http://localhost:5000/api';
+const String localIp = 'https://shervice-python-production.up.railway.app';
+const String backendUrl =
+    'https://shervice-python-production.up.railway.app/api';
